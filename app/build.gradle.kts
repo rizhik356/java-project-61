ed4cab42bc067223d24e9b72278508870dbd64ee
Alex Kalinin <cyrex228@icloud.com>
2026-01-15 19:42:50 +0300

    .

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,10 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.53.0"
+    application
+}
+application {
+    mainClass.set("hexlet.code.App")
 }
 
 group = "hexlet.code"
